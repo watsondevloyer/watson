7678199f1dda8f7eaf5013a974c2227393e3deed
guochang.xie <guochang.xie@cardinfo.com.cn>
2019-04-29 10:37:38 +0800

    增加测试代码

--- watson-uitest/src/test/java/com/watson/uitest/BaiduPage.java
+++ watson-uitest/src/test/java/com/watson/uitest/BaiduPage.java
@@ -2,14 +2,13 @@ package com.watson.uitest;
 import com.watson.uitest.struct.element.Element;
 import com.watson.uitest.struct.element.locator.annotation.*;
 import com.watson.uitest.struct.Page;
-import lombok.Getter;
 
 /**
  * @author guochang.xie
  * @Description: TODO
  * @date 2019/4/269:36 AM
  */
-@Getter
+
 public class BaiduPage extends Page {
 
   @ID("kw")
--- watson-uitest/src/test/java/com/watson/uitest/WatsonWindowTest.java
+++ watson-uitest/src/test/java/com/watson/uitest/WatsonWindowTest.java
@@ -1,7 +1,6 @@
 package com.watson.uitest;
 
 import com.watson.uitest.window.EngineType;
-import lombok.Setter;
 import org.junit.Test;
 
 
@@ -12,15 +11,13 @@ import org.junit.Test;
  * @version 1.0
  * @since <pre>Apr 25, 2019</pre>
  */
-@Setter
+
 public class WatsonWindowTest {
-
-
 
     private  BaiduPage baiduPage=new BaiduPage();
 
     @Test
-    public void testOpenNewWindow() throws Exception {
+    public void testOpenNewWindow() {
         WatsonWindow.openNewWindow(EngineType.WebDriverSafari,"http:www.baidu.com");
         baiduPage.search("百度输入框测试");
         WatsonWindow.closeAllWindow();
